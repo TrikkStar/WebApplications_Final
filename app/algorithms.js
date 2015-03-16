ef303c0efcb844bed15edb4578957fe79d378f0c
Noah Thederahn <thederahnn16@hanover.edu>
2015-03-15 21:14:30 -0400

    modified eslint to closer match desired style, added insertion sort to algorithms.js

--- app/algorithms.js
+++ app/algorithms.js
@@ -1,13 +1,13 @@
 var algorithms = {
-	selection: function(arr, type) {
-	    var len, cpy;
+	selection: function(arr, type){
+	    var len, cpy, i;
 	    len = arr.length;
 	    cpy = arr.slice();
-	    for(var i=0; i<len-1; i++){
-	        var min, temp;
+	    for (i = 0; i < len - 1; i += 1){
+	        var min, temp, j;
 	        min = i;
-	        for(var j=i+1; j<len; j++){
-	            if (cpy[j].localeCompare(cpy[min]) < 0){ //need to add logic for chosing type of data in cpy
+	        for (j = i + 1; j < len; j += 1){
+	            if (cpy[j].localeCompare(cpy[min]) < 0){ // need to add logic for chosing type of data in cpy
 	                min = j;
 	            }
 	        }
@@ -16,5 +16,23 @@ var algorithms = {
 	        cpy[min] = temp;
 	    }
 	    return cpy;
+	},
+	insertion: function(arr, type){
+		var cpy, i;
+		cpy = arr.slice();
+		for (i = 1; i < cpy.length; i += 1){
+			var u, j;
+			u = cpy[i];
+			j = i - 1;
+			while (j >= 0 && cpy[j].localeCompare(u) > 0){ // add logic for chosing type
+			    cpy[j + 1] = cpy[j];
+			    j -= 1;
+			}
+			cpy[j + 1] = u;
+		}
+		return cpy;
+	},
+	mergesort: function(arr, type){
+		// to be added later
 	}
-}
+};
