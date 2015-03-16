329dc9982a04be469ffaec6a69b697ca648586ce
Noah Thederahn <thederahnn16@hanover.edu>
2015-03-15 20:37:07 -0400

    added require files

---
+++ require.js
@@ -0,0 +1,6 @@
+
+var Handlebars, Algorithms, Main, jQuery;
+Handlebars = require("./lib/handlebars-v3.0.0");
+jQuery = require("./lib/jquery-2.1.3");
+Algorithms = require("./app/algorithms");
+Main = require("./app/main");
