82471c73762cfea057766c15fea774a11b8a1e63
Noah Thederahn <thederahnn16@hanover.edu>
2015-03-19 12:00:49 -0400

    more framework added, learing how to implient handlebars (#3)

--- app/interface.js
+++ app/interface.js
@@ -1,11 +1,13 @@
 
-define(["./algorithms"], function(algorithms){
-var arr = [5, 7, 8, 3, 6, 2, 4, 9, 0, 1];
+define(["./algorithms", "../lib/handlebars-v3.0.0"], function(algorithms){
+	var arr, controller;
+	// arr = [5, 7, 8, 3, 6, 2, 4, 9, 0, 1];
+	// console.log(Algorithms.selection(arr, "number"));
+	// console.log(Algorithms.insertion(arr, "number"));
+	// console.log(Algorithms.mergesort(arr, "number")); mergesort is not functioning properly, it crashes the browser
+	// console.log(Algorithms.quicksort(arr, "number"));
+	
+	controller = function(){
 
-// console.log(Algorithms.selection(arr, "number"));
-// console.log(Algorithms.insertion(arr, "number"));
-// console.log(Algorithms.mergesort(arr, "number")); mergesort is not functioning properly, it crashes the browser
-// console.log(Algorithms.quicksort(arr, "number"));
-
-
+	};
 });
