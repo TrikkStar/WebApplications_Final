5f2af3291572fb2c5385557172e047007e141a27
Noah Thederahn <thederahnn16@hanover.edu>
2015-03-19 11:13:06 -0400

    all algorithms except mergesort working properly (#4)

--- app/interface.js
+++ app/interface.js
@@ -2,10 +2,10 @@
 define(["./algorithms"], function(algorithms){
 var arr = [5, 7, 8, 3, 6, 2, 4, 9, 0, 1];
 
-console.log(Algorithms.selection(arr, "number"));
-console.log(Algorithms.insertion(arr, "number"));
-console.log(Algorithms.mergesort(arr, "number"));
-console.log(Algorithms.quicksort(arr, "number"));
+// console.log(Algorithms.selection(arr, "number"));
+// console.log(Algorithms.insertion(arr, "number"));
+// console.log(Algorithms.mergesort(arr, "number")); mergesort is not functioning properly, it crashes the browser
+// console.log(Algorithms.quicksort(arr, "number"));
 
 
 });
