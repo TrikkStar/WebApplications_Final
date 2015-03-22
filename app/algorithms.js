2e6a8dc4cd09c1b2e52c7f9ec606563cc252e9cc
Noah Thederahn <thederahnn16@hanover.edu>
2015-03-22 16:52:51 -0400

    added comparitor function to handle compairing elements against each other, fixed problem with partition that was made apparent through this change

--- app/algorithms.js
+++ app/algorithms.js
@@ -1,3 +1,22 @@
+var Algorithms, algArray;
+
+function comparitor(a, b){
+	if (typeof a === "string" && typeof b === "string"){
+		return a.localeCompare(b);
+	} else if (typeof a === "number" && typeof b === "number"){
+		if (a < b){
+			return -1;
+		} else if (a === b){
+			return 0;
+		} else {
+			return 1;
+		}
+	} else if (typeof a !== typeof b) {
+		throw new Error("Units not of same type");
+	} else {
+		throw new Error("Invalid unit type");
+	}
+}
 
 function arrayCopy(src, srcPos, dest, destPos, len){
 	var temp;
@@ -10,13 +29,12 @@ function arrayCopy(src, srcPos, dest, destPos, len){
 	}
 }
 
-function merge(arrB, arrC, arrA, type){
+function merge(arrB, arrC, arrA){
     var i = 0, j = 0, k = 0, p, q, temp;
     p = arrB.length;
     q = arrC.length;
     while (i < p && j < q){
-	if (type === "string"){
-		if (arrB[i].localeCompare(arrC[j]) <= 0){
+		if (comparitor(arrB[i], arrC[j]) <= 0){
 			temp = arrB[i];
 			arrA[k] = temp;
 			i += 1;
@@ -25,17 +43,6 @@ function merge(arrB, arrC, arrA, type){
 			arrA[k] = temp;
 			j += 1;
 		}
-	} else {
-		if (arrB[i] <= arrC[j]){
-			temp = arrB[i];
-			arrA[k] = temp;
-			i += 1;
-		} else {
-			temp = arrC[j];
-			arrA[k] = temp;
-			j += 1;
-		}
-	}
         k += 1;
     }
     if (i === p){
@@ -45,29 +52,21 @@ function merge(arrB, arrC, arrA, type){
     }
 }
 
-function partition(arr, left, right, type){
-	var temp, pivot;
-	pivot = arr[(left + right) / 2];
+function partition(arr, left, right){
+	var temp1, temp2, pivot;
+	pivot = arr[Math.floor((left + right) / 2)];
 	while (left <= right){
-		if (type === "string"){
-			while (arr[left].localeCompare(pivot) < 0){
-				left += 1;
-			}
-			while (arr[right].localeCompare(pivot) > 0){
-				right += 1;
-			}
-		} else {
-			while (arr[left] < pivot){
-				left += 1;
-			}
-			while (arr[right] > pivot){
-				right += 1;
-			}
+		while (comparitor(arr[left], pivot) < 0){
+			left += 1;
+		}
+		while (comparitor(arr[right], pivot) > 0){
+			right -= 1;
 		}
 		if (left <= right){
-			temp = arr[left];
-			arr[left] = arr[right];
-			arr[right] = temp;
+			temp1 = arr[left];
+			temp2 = arr[right];
+			arr[left] = temp2;
+			arr[right] = temp1;
 			left += 1;
 			right -= 1;
 		}
@@ -75,8 +74,8 @@ function partition(arr, left, right, type){
 	return left;
 }
 
-function quick(arr, left, right, type){
-	var index = partition(arr, left, right, type);
+function quick(arr, left, right){
+	var index = partition(arr, left, right);
 	if (left < index - 1){
 		quick(arr, left, index - 1);
 	}
@@ -85,8 +84,8 @@ function quick(arr, left, right, type){
 	}
 }
 
-var Algorithms = {
-	selection: function(arr, type){
+Algorithms = {
+	selection: function(arr){
 	    var len, cpy, i;
 	    len = arr.length;
 	    cpy = arr.slice();
@@ -94,14 +93,8 @@ var Algorithms = {
 	        var min, temp, j;
 	        min = i;
 	        for (j = i + 1; j < len; j += 1){
-				if (type === "string"){ // need to determine a better way to select type comparison, possibly determining type at runtime
-					if (cpy[j].localeCompare(cpy[min]) < 0){
-						min = j;
-					}
-				} else {
-					if (cpy[j] < cpy[min]){
-						min = j;
-					}
+				if (comparitor(cpy[j], cpy[min]) < 0){
+					min = j;
 				}
 	        }
 	        temp = cpy[i];
@@ -110,29 +103,22 @@ var Algorithms = {
 	    }
 	    return cpy;
 	},
-	insertion: function(arr, type){
+	insertion: function(arr){
 		var cpy, i;
 		cpy = arr.slice();
 		for (i = 1; i < cpy.length; i += 1){
 			var u, j;
 			u = cpy[i];
 			j = i - 1;
-			if (type === "string"){
-				while (j >= 0 && cpy[j].localeCompare(u) > 0){
-				    cpy[j + 1] = cpy[j];
-				    j -= 1;
-				}
-			} else {
-				while (j >= 0 && cpy[j] > u){
-				    cpy[j + 1] = cpy[j];
-				    j -= 1;
-				}
+			while (j >= 0 && comparitor(cpy[j], u) > 0){
+			    cpy[j + 1] = cpy[j];
+			    j -= 1;
 			}
 			cpy[j + 1] = u;
 		}
 		return cpy;
 	},
-	mergesort: function(arrA, type){ // Figure out way for recursive searches to return a copy of the original array
+	mergesort: function(arrA){ // Figure out way for recursive searches to return a copy of the original array
 	    var n, arrB, arrC;
 	    n = arrA.length;
 	    if (n > 1){
@@ -144,15 +130,15 @@ var Algorithms = {
 	        } else {
 	            arrayCopy(arrA, n / 2, arrC, 0, n / 2 + 1);
 	        }
-	        arrB = Algorithms.mergesort(arrB, type);
-	        arrC = Algorithms.mergesort(arrC, type);
-	        merge(arrB, arrC, arrA, type);
+	        arrB = Algorithms.mergesort(arrB);
+	        arrC = Algorithms.mergesort(arrC);
+	        merge(arrB, arrC, arrA);
 	    }
 	    return arrA;
 	},
-	quicksort: function(arr, type){
+	quicksort: function(arr){
 		var cpy = arr.slice();
-		quick(cpy, 0, cpy.length - 1, type);
+		quick(cpy, 0, cpy.length - 1);
 		return cpy;
 	}
 };
--- app/interface.js
+++ app/interface.js
@@ -1,11 +1,15 @@
 
-define(["./algorithms", "../lib/handlebars-v3.0.0"], function(algorithms){
-	var arr, controller;
-	// arr = [5, 7, 8, 3, 6, 2, 4, 9, 0, 1];
-	// console.log(Algorithms.selection(arr, "number"));
-	// console.log(Algorithms.insertion(arr, "number"));
-	// console.log(Algorithms.mergesort(arr, "number")); mergesort is not functioning properly, it crashes the browser
-	// console.log(Algorithms.quicksort(arr, "number"));
+define(["./algorithms"], function(algorithms){
+	var arr, arr2, controller;
+	arr = [5, 7, 8, 3, 6, 2, 4, 9, 0, 1];
+	arr2 = ["x", "q", "p", "w", "a", "c", "n"];
+	// console.log(Algorithms.selection(arr));
+	// console.log(Algorithms.selection(arr2));
+	// console.log(Algorithms.insertion(arr));
+	// console.log(Algorithms.insertion(arr2));
+	// console.log(Algorithms.mergesort(arr)); mergesort is not functioning properly, it crashes the browser
+	// console.log(Algorithms.quicksort(arr));
+	// console.log(Algorithms.quicksort(arr2));
 	
 	controller = function(){
 
